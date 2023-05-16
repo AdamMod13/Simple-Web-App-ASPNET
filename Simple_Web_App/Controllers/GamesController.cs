d20b13e2adf5f67359b2f2310b1f919dffa9ff01
Modlich Adam <adam.modlich13@gmail.com>
2023-05-16 11:16:40 +0200

    SWA-2: Adding web logo and not working sorting

---
+++ Simple_Web_App/Controllers/GamesController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Simple_Web_App;
+using Simple_Web_App.Data;
+
+namespace Simple_Web_App.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly Simple_Web_AppContext _context;
+
+        public GamesController(Simple_Web_AppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Games
+        public async Task<IActionResult> Index()
+        {
+              return _context.Game != null ? 
+                          View(await _context.Game.ToListAsync()) :
+                          Problem("Entity set 'Simple_Web_AppContext.Game'  is null.");
+        }
+
+        public IActionResult Sort(string sortOrder)
+        {
+            ViewBag.TitleSortParam = string.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.ReleaseDateSortParam = sortOrder == "releaseDate" ? "releaseDate_desc" : "releaseDate";
+            ViewBag.PriceSortParam = sortOrder == "price" ? "price_desc" : "price";
+
+            var games = from g in _context.Game select g;
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    games = games.OrderByDescending(g => g.Title);
+                    break;
+                case "price":
+                    games = games.OrderBy(g => g.Price);
+                    break;
+                case "price_desc":
+                    games = games.OrderByDescending(g => g.Price);
+                    break;
+                case "releaseDate":
+                    games = games.OrderBy(g => g.RelaseDate);
+                    break;
+                case "releaseDate_desc":
+                    games = games.OrderByDescending(g => g.RelaseDate);
+                    break;
+                default:
+                    games = games.OrderBy(g => g.Title);
+                    break;
+            }
+
+            return View("Index", games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Game == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,RelaseDate,Genre,Platforms,Price")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(game);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Game == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,RelaseDate,Genre,Platforms,Price")] Game game)
+        {
+            if (id != game.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(game);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Game == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Game == null)
+            {
+                return Problem("Entity set 'Simple_Web_AppContext.Game'  is null.");
+            }
+            var game = await _context.Game.FindAsync(id);
+            if (game != null)
+            {
+                _context.Game.Remove(game);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameExists(int id)
+        {
+          return (_context.Game?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
